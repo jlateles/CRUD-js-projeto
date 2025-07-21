b284e1cfd3d7073cc5c9df8055bb2afb216afbbf
jlateles <juliatelesicm@gmail.com>
2025-07-20 22:09:47 -0300

    first commit

---
+++ js/api.js
@@ -0,0 +1,72 @@
+const api = {
+  
+  async buscarPensamentos() {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos')
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
+
+   async salvarPensamentos(pensamento) {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao salvar pensamentos')
+      throw error
+    }
+  },
+  
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao editar pensamento')
+      throw error
+    }
+  },
+
+    async excluirPensamento(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+        method: "DELETE"
+      })
+    }
+    catch {
+      alert('Erro ao excluir pensamento')
+      throw error
+    }
+  }
+
+}
+export default api
